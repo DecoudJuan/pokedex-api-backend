8185704c6578ac369f9ef91c6b9f5dd4551a13aa
DecoudJuan <decoudjuanmanuel1@gmail.com>
2025-08-17 19:45:24 -0300

    Railway fixes v3

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PokemonModule } from './pokemon/pokemon.module';
+import { HealthController } from './healthz/healthz.controller';
+
 
 @Module({
   imports: [PokemonModule],
-  controllers: [AppController],
+  controllers: [AppController, HealthController],
   providers: [AppService],
 })
 export class AppModule {}
