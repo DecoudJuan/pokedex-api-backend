2766471ec7e6faa03da94a2e9b7bb802a439ffdd
DecoudJuan <decoudjuanmanuel1@gmail.com>
2025-08-18 14:07:54 -0300

    Local permissed in Prod

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,22 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const whitelist = [
+    'http://localhost:5173',              // frontend en dev
+    'https://pokedex-api-app.vercel.app'  // frontend en prod
+  ];
+  
   app.enableCors({
-    origin: process.env.FRONTEND_URL ?? '*',
+    origin: (origin, cb) => {
+      if (!origin) return cb(null, true); // permite curl / Postman
+      if (whitelist.includes(origin)) return cb(null, true);
+      return cb(new Error(`CORS bloqueado para ${origin}`), false);
+    },
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   });
-
-  const port = Number(process.env.PORT) || 3000;
-  await app.listen(port, '0.0.0.0'); 
+  
 
 }
 bootstrap();
